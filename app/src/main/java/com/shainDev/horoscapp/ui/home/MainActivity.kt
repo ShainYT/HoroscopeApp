0d2541e6c3d649a95df2c2566d8bfdfacf1a83ee
mrrobot <201760089@ucc.mx>
2023-12-26 15:12:16 -0600

    initial commit

---
+++ app/src/main/java/com/shainDev/horoscapp/ui/home/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.shainDev.horoscapp.ui.home
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.shainDev.horoscapp.R
+import com.shainDev.horoscapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initUI()
+    }
+
+    private fun initUI() {
+        initNavigation()
+    }
+
+    private fun initNavigation() {
+        val navHost =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment //Se especifica el tipo para que no quede como generico
+        navController = navHost.navController
+        binding.buttonNavView.setupWithNavController(navController)
+    }
+}
