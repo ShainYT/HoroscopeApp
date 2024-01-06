dbb32ccbe2d2602c8173a141a9b6645e46879256
Alvaro Cruz <201760089@ucc.mx>
2024-01-06 12:57:20 -0600

    LuckView Completed

--- app/src/main/java/com/shainDev/horoscapp/ui/luck/LuckFragment.kt
+++ app/src/main/java/com/shainDev/horoscapp/ui/luck/LuckFragment.kt
@@ -1,6 +1,8 @@
 package com.shainDev.horoscapp.ui.luck
 
 import android.animation.ObjectAnimator
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -14,8 +16,12 @@ import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import com.shainDev.horoscapp.R
 import com.shainDev.horoscapp.databinding.FragmentLuckBinding
+import com.shainDev.horoscapp.ui.core.listeners.OnSwipeTouchListener
+import com.shainDev.horoscapp.ui.model.LuckyModel
+import com.shainDev.horoscapp.ui.providers.RandomCardProvider
 import dagger.hilt.android.AndroidEntryPoint
 import java.util.Random
+import javax.inject.Inject
 
 
 @AndroidEntryPoint
@@ -25,19 +31,56 @@ class LuckFragment : Fragment() {
     private var _binding: FragmentLuckBinding? = null
     private val binding get() = _binding!!
 
+    @Inject
+    lateinit var randomCardProvider: RandomCardProvider
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initUI()
     }
 
     private fun initUI() {
+        preparePrediction()
         initListeners()
     }
 
-    private fun initListeners() {
-        binding.ivRoulette.setOnClickListener {
-            spinRoulette()
+    private fun preparePrediction() {
+        val currentLuck = randomCardProvider.getLucky()
+        currentLuck?.let { luck ->
+            val currentPrediction = getString(luck.text)
+            binding.tvLucky.text = currentPrediction
+            binding.ivLuckyCard.setImageResource(luck.image)
+            binding.tvShare.setOnClickListener {
+                shareResult(currentPrediction)
+            }
         }
+    }
+
+    private fun shareResult(prediction: String) {
+        val sendIntent: Intent = Intent().apply {
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_TEXT, prediction)
+            type = "text/plain"
+        }
+
+        val shareIntent = Intent.createChooser(sendIntent, null)
+        startActivity(shareIntent)
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private fun initListeners() {
+//        binding.ivRoulette.setOnClickListener {
+//            spinRoulette()
+//        }
+        binding.ivRoulette.setOnTouchListener(object : OnSwipeTouchListener(requireContext()) {
+            override fun onSwipeRight() {
+                spinRoulette()
+            }
+
+            override fun onSwipeLeft() {
+                spinRoulette()
+            }
+        })
     }
 
     private fun spinRoulette() {
@@ -115,6 +158,9 @@ class LuckFragment : Fragment() {
 
             }
         })
+
+        val example = LuckyModel(R.drawable.tauro, R.string.aries)
+
         binding.preview.startAnimation(disappearAnimation)
         binding.prediction.startAnimation(appearAnimation)
     }
